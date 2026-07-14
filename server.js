4e4f7787a3e5ba8de21a31e8d15ebbceabc488eb
Some-Dude-Bra <Padrakegaming@gmail.com>
2026-07-14 12:47:49 +0800

    etyui

--- server.js
+++ server.js
@@ -899,6 +899,7 @@ app.use((err, req, res, next) => {
 });
 
 // ─── START ────────────────────────────────────────────────────────────────────
+require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
