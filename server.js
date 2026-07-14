fc02923d7f928a8bf73742780f5d9df92efab7e5
Some-Dude-Bra <Padrakegaming@gmail.com>
2026-07-14 12:23:42 +0800

    welp i'm idiot now

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 // ─── DEPENDENCIES ────────────────────────────────────────────────────────────
-const express = require('express');
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+app.use(express.static(__dirname));   // <-- Add this
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 const mysql   = require('mysql2');
 const cors    = require('cors');
 const multer  = require('multer');
